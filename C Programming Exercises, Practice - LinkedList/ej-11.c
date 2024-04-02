1a72c59e47ab405fcb44cd86adcad8612a4347ef
Guido Pastorino <guidopasto05@gmail.com>
2024-04-02 10:05:13 -0300

    .

--- C Programming Exercises, Practice - LinkedList/ej-11.c
+++ C Programming Exercises, Practice - LinkedList/ej-11.c
@@ -1,3 +1,16 @@
+// Write a C program to find the intersection of two singly linked lists.
+// Test Data and Expected Output :
+
+// Original lists:
+// 1 2 3 4 
+// 5 3 4 
+// Intersection found at 3.
+
+// Original lists:
+// 1 2 3 4 
+// 5 3 4 
+// Intersection not found.
+
 #include "SList_Funcs.h"
 
 Node* getIntersection(Node* head1, Node* head2){
