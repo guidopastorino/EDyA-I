9b6fb41b3bef60fd670da44121a9b7f78952b820
Guido Pastorino <guidopasto05@gmail.com>
2024-05-07 21:30:39 -0300

    Updating insertion sort

--- sorting algos/insertionsort.c
+++ sorting algos/insertionsort.c
@@ -1,77 +1,22 @@
 #include "ArrFuncs.h"
 
-typedef struct _SNode {
-  int data;
-  struct _SNode* next;
-} SNode;
-
-SNode* create_node(int data){
-  SNode* newNode = (SNode*)malloc(sizeof(SNode));
-  assert(newNode != NULL);
-  newNode->data = data;
-  newNode->next = NULL;
-  return newNode;
-}
-
-SNode* insert_end(SNode* head, int data){
-  SNode* newNode = create_node(data);
-  if(head == NULL){
-    head = newNode;
-  } else {
-    SNode* current = head;
-    while(current->next != NULL){
-      current = current->next;
+void insertion_sort(int arr[], int n){
+  int i, j, key;
+  for(int i = 1; i<n; i++){
+    key = arr[i];
+    j = i - 1;
+    while(j >= 0 && key < arr[j]){
+      arr[j + 1] = arr[j];
+      j--;
     }
-    current->next = newNode;
+    arr[j + 1] = key;
   }
-  return head;
-}
-
-void display_list(SNode* head){
-  while(head != NULL){
-    printf("%d -> ", head->data);
-    head = head->next;
-  }
-  printf("NULL\n");
-}
-
-void insertionSort(SNode** head){
-  if (*head == NULL || (*head)->next == NULL) return;
-
-  SNode* sorted = NULL;
-  SNode* current = *head;
-
-  while (current != NULL) {
-    SNode* next = current->next;
-    if (sorted == NULL || current->data < sorted->data) {
-      current->next = sorted;
-      sorted = current;
-    } else {
-      SNode* search = sorted;
-      while (search->next != NULL && search->next->data < current->data) {
-        search = search->next;
-      }
-      current->next = search->next;
-      search->next = current;
-    }
-    current = next;
-  }
-  *head = sorted;
 }
 
 int main(){
-  SNode* head = NULL;
-  head = insert_end(head, 45);
-  head = insert_end(head, 30);
-  head = insert_end(head, 60);
-  head = insert_end(head, 20);
-  head = insert_end(head, 15);
-
-  display_list(head);
-
-  insertionSort(&head);
-
-  display_list(head);
+  printArray(arr, size);
+  insertion_sort(arr, size);
+  printArray(arr, size);
 
   return 0;
 }
