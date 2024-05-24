58c71697aba7f3291b954449eb3e019ec13fc9bc
Guido Pastorino <guidopasto05@gmail.com>
2024-05-24 14:11:54 -0300

    añadiendo ejercicios

---
+++ practica 4/avltree/avl.h
@@ -0,0 +1,74 @@
+#ifndef __AVL_H__
+#define __AVL_H__
+
+typedef void *(*FuncionCopiadora)(void *dato);
+typedef int (*FuncionComparadora)(void *, void *);
+typedef void (*FuncionDestructora)(void *dato);
+typedef void (*FuncionVisitanteExtra)(void *dato, void *extra);
+
+typedef enum {
+  AVL_RECORRIDO_IN,  /** Inorden */
+  AVL_RECORRIDO_PRE, /** Preorden */
+  AVL_RECORRIDO_POST /** Postorden */
+} AVLRecorrido;
+
+typedef struct _AVL *AVL;
+
+
+/**
+ * Estructura del nodo del arbol AVL.
+ * Tiene un puntero al dato (dato),
+ * un puntero al nodo raiz del subarbol izquierdo (izq),
+ * un puntero al nodo raiz del subarbol derecho (der), y
+ * un entero para representar la altura del arbol (altura)
+ */
+typedef struct _AVL_Nodo {
+  void* dato;
+  struct _AVL_Nodo* izq, * der;
+  int altura;
+} AVL_Nodo;
+
+/**
+ * Retorna un arbol AVL vacio
+ */
+AVL avl_crear(FuncionCopiadora, FuncionComparadora, FuncionDestructora);
+
+/**
+ * Destruye el arbol y sus datos.
+ */
+void avl_destruir(AVL);
+
+/**
+ * Retorna 1 si el dato se encuentra y 0 en caso contrario
+ */
+int avl_buscar(AVL, void *);
+
+/**
+ * Inserta un dato no repetido en el arbol, manteniendo la propiedad de los
+ * arboles AVL.
+ */
+void avl_insertar(AVL, void *);
+
+/**
+ * Encuentra el nodo con el valor mínimo en el subárbol
+ */
+AVL_Nodo* encontrar_minimo(AVL_Nodo* raiz);
+
+/**
+ * Elimina un nodo en el árbol si es que está presente, manteniendo la propiedad
+ * de los árboles AVL.
+ */
+void avl_eliminar(AVL, void *);
+
+/**
+ * Retorna 1 si el arbol cumple la propiedad de los arboles AVL, y 0 en caso
+ * contrario.
+ */
+int avl_validar(AVL);
+
+/**
+ * Recorrido DSF del arbol
+ */
+void avl_recorrer(AVL, AVLRecorrido, FuncionVisitanteExtra, void *);
+
+#endif /* __AVL_H__*/
